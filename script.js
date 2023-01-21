9b121c732dc0bcbf494b0ea785622ed84c0579f7
Ethan-Ryan-Anderson <118407438+Ethan-Ryan-Anderson@users.noreply.github.com>
2023-01-21 01:27:05 -0500

    Output and Multiple Operands
    Actual GUI output now, along with the feature to have multiple operands such as "5+5+5"

--- script.js
+++ script.js
@@ -3,21 +3,53 @@ var subnum = '';
 var postoperand = false;
 var operand = '';
 var decamount = 0;
-var operandcnt = 1
-
-
+var operandcnt = 0;
+var output = document.getElementById('output');
+var outputstring = '';
 
 
 function opperand(opp) {
-    operand = opp;
-    operandcnt++;
+    if (operandcnt >= 1) {
+        equals();
+        operand = opp;
+        operandcnt++;
+    }
+    else {
+        operand = opp;
+        operandcnt++;
     }
 
-function number(num) {
-    if (operandcnt % 2 == !0) {
-    mainnum = mainnum + num;
-        console.log(mainnum);
 }
+
+    /////////////////
+function number(num) {
+    if (operandcnt == 0) {
+        mainnum = mainnum + num;
+        console.log(mainnum);
+       // output.innerHTML = mainnum;
+    }
+
+   else if (operandcnt % 2 == 0) {
+       mainnum = Number(mainnum) + Number(num);
+       console.log(mainnum);
+       operand = '';
+       // output.innerHTML = mainnum;
+    }
+    else if (operandcnt % 2 == !0) {
+    subnum = subnum + num;
+         console.log(subnum);
+        //output.innerHTML = mainnum;
+    }
+}
+
+function outputtext() {
+    if (mainnum === Infinity || mainnum === -Infinity || mainnum === NaN || mainnum === -NaN) {
+        output.innerHTML = 'You did some dumbshit, like divide by zero';
+    }
+    else {
+    outputstring = mainnum + operand + subnum;
+    output.innerHTML = outputstring;
+    }
 }
 
 function ac() {
@@ -31,26 +63,34 @@ function equals(a) {
     if (operand == '+') {
         var sum = Number(mainnum) + Number(subnum);
         console.log(sum);
+       // output.innerHTML = sum;
         mainnum = sum.toString();
         subnum = '';
+        operand = '';
     }
     else if (operand == '-') {
         var sum = Number(mainnum) - Number(subnum);
         console.log(sum);
+      //  output.innerHTML = sum;
         mainnum = sum.toString();
         subnum = '';
+        operand = '';
     }
     else if (operand == '*') {
         var sum = Number(mainnum) * Number(subnum);
         console.log(sum);
+      //  output.innerHTML = sum;
         mainnum = sum.toString();
         subnum = '';
+        operand = '';
     }
     else if (operand == '/') {
         var sum = Number(mainnum) / Number(subnum);
         console.log(sum);
+      //  output.innerHTML = sum;
         mainnum = sum.toString();
         subnum = '';
+        operand = '';
             }
 
 }
